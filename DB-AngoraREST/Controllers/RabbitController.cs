32e45219455591532856b2f80e87c8589865f6b8
Mikkel Nørfeldt Friborg <mikk908w@edu.zealand.dk>
2024-04-20 00:34:39 +0200

    Lille update..

--- DB-AngoraREST/Controllers/RabbitController.cs
+++ DB-AngoraREST/Controllers/RabbitController.cs
@@ -1,5 +1,6 @@
 ﻿using DB_AngoraLib.Models;
 using DB_AngoraLib.Services.RabbitService;
+using DB_AngoraLib.Services.UserService;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -10,19 +11,21 @@ namespace DB_AngoraREST.Controllers
     public class RabbitController : ControllerBase
     {
         private readonly IRabbitService _rabbitService;
+        private readonly IUserService _userService;
 
-        public RabbitController(IRabbitService rabbitService)
+        public RabbitController(IRabbitService rabbitService, IUserService userService)
         {
             _rabbitService = rabbitService;
+            _userService = userService;
         }
 
-
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
         [HttpGet]
         public async Task<ActionResult<IEnumerable<Rabbit>>> GetRabbits()
         {
             var rabbits = await _rabbitService.GetAllRabbitsAsync();
             return Ok(rabbits);
         }
-
     }
 }
